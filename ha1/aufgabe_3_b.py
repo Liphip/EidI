e59583c8bbe11f8dc85a8f3b1587afe27fb972c3
Philip Laskowicz <lord.pumpkin@web.de>
2019-10-28 10:01:21 +0100

    A3_b Verbessert

--- ha1/aufgabe_3_b.py
+++ ha1/aufgabe_3_b.py
@@ -1,19 +1,6 @@
-try:
-    x = int(input("Geben sie die Stunden ihrer geschätzten Ankuftszeit an > "))
-    y = int(input("Geben sie die Minuten ihrer geschätzten Ankuftszeit an > "))
-except:
-    print("Bitte neustarten und zweistelligen Zahlenwert mit 0<=Stunden<=23 und 0<=Minuten<=59 eingeben!")
-    exit(-1)
+x = int(input("Geben sie die Stunden ihrer geschätzten Ankuftszeit an > "))
+y = int(input("Geben sie die Minuten ihrer geschätzten Ankuftszeit an > "))
 
-if 23 <= x <= 0 or 59 < y < 0:
-    print("Bitte neustarten und zweistelligen Zahlenwert mit 0<=Stunden<=23 und 0<=Minuten<=59 eingeben!")
-    exit(-1)
-
-timeMin = ((x * 60) + y)
-timeGE = (timeMin - 420)
-
-if (timeGE > 0):
-    print("%02d:%02d" % (timeGE // 60, timeGE % 60))
-else:
-    timeGE = (1440 + timeGE)
-    print("%02d:%02d" % (timeGE // 60, timeGE % 60))
+timeGE = (((x * 60) + y) - (7*60))
+timeGE %= 24 * 60
+print("%02d:%02d" % (timeGE // 60, timeGE % 60))
