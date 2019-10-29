43f89bac39e90aecd56c81370bfec91424d28aae
Luis Michaelis <private@lmichaelis.de>
2019-10-29 13:33:32 +0100

    A2 Verbessert

--- ha1/aufgabe_2.py
+++ ha1/aufgabe_2.py
@@ -1,8 +1,4 @@
 x = input("Geben sie eine zweistellige Zahl an > ")
 
-if len(x) != 2:
-    print("Bitte neustarten und zeistellien Zahlenwert eingeben!")
-    exit(-1)
-
 out = int(x[0]) + int(x[1])
 print(str(out) * out)
