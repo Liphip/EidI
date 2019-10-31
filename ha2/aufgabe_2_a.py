2444e92d764651637123d0415eaf67706aff819d
Luis Michaelis <private@lmichaelis.de>
2019-10-31 18:26:16 +0100

    Ha2: A2

---
+++ ha2/aufgabe_2_a.py
@@ -0,0 +1,10 @@
+"""
+Copyright (c) 2019  Luis Michaelis, Philip Laskowicz
+Licensed under MIT (https://opensource.org/licenses/mit-license.php).
+"""
+
+inp = [ord(i) for i in input('Encrypt > ')]
+offset = int(input('Offset > '))
+
+out = ''.join([chr((i + offset) % 128) for i in inp])
+print(out)
