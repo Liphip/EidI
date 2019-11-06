17085ea2b8816c6e5b943ebf7ae651bbb3d9dca3
Liphip <57034143+Liphip@users.noreply.github.com>
2019-11-06 07:51:21 +0100

    Update aufgabe_2_a.py

--- ha2/aufgabe_2_a.py
+++ ha2/aufgabe_2_a.py
@@ -2,6 +2,6 @@ st = input('Bitte geben sie eine Zeichenkette zum Verschlüsseln ein >>>')
 enc = int(input('Bitte geben sie den Encoding Integer ein >>>'))
 out = ''
 for i in st:
-    out += chr((ord(i) + enc) % 127)
+    out += chr((ord(i) + enc) % 128)
 
 print(out)
