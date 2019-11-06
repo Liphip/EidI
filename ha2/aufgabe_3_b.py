4964f5a65774a566ecc024206bc8492b5bc8c2b2
Liphip <57034143+Liphip@users.noreply.github.com>
2019-11-06 07:35:24 +0100

    Update aufgabe_3_b.py

--- ha2/aufgabe_3_b.py
+++ ha2/aufgabe_3_b.py
@@ -5,15 +5,10 @@ except:
     exit(-1)
 
 bin = ''
-gerade = False
-
-if dec % 2 == 0:
-    gerade = True
 
 while dec > 1:
     bin += str(dec % 2)
     dec = dec // 2
-    print(bin)
 
 bin = '1' + bin[::-1]
 
