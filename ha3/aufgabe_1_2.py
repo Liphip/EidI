e822e38caf682b2c640d3dc477d854fabb5b4a6e
Philip Laskowicz <lord.pumpkin@web.de>
2019-11-13 10:27:40 +0100

    Fixed aufgabe_1.py -> can´t read umlaut without encoding='utf-8'

--- ha3/aufgabe_1_2.py
+++ ha3/aufgabe_1_2.py
@@ -5,7 +5,7 @@ Licensed under MIT (https://opensource.org/licenses/mit-license.php).
 
 word = input('Geben sie das Wort ein, das sie suchen > ')
 
-with open('dictionary.txt', 'r') as read:
+with open('dictionary.txt', 'r', encoding='utf-8') as read:
     lines = read.readlines()
 
     for i in range(len(lines)):
