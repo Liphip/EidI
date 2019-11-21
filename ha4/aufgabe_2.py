b3b01bfc6892f4bf1261205b0a655b1bc05e3275
Philip Laskowicz <lord.pumpkin@web.de>
2019-11-21 12:13:42 +0100

    Fixed Format

--- ha4/aufgabe_2.py
+++ ha4/aufgabe_2.py
@@ -6,16 +6,16 @@ Licensed under MIT (https://opensource.org/licenses/mit-license.php).
 
 def intSuperliste(lst: list) -> bool:
     save_type = None
-    
+
     for element in lst:
         if save_type is None:
             save_type = type(element)
-        
+
         if type(element) == list and not intSuperliste(element):
             return False
         elif (type(element) != list and type(element) != int) or type(element) != save_type:
             return False
-    
+
     return True
 
 
--- ha4/aufgabe_3.py
+++ ha4/aufgabe_3.py
@@ -4,11 +4,11 @@ Licensed under MIT (https://opensource.org/licenses/mit-license.php).
 """
 
 
-def height(x: float) -> float:
-    a = x if x - 34 <= 0 else height(x - 34)
-    b = x if (x - 11) / 2 <= 0 else height((x - 11) / 2)
-    
+def hoehe(x: float) -> float:
+    a = x if x - 34 <= 0 else hoehe(x - 34)
+    b = x if (x - 11) / 2 <= 0 else hoehe((x - 11) / 2)
+
     return a if a < b else b
 
 
-print(height(float(input('Höhe > '))))
+print(hoehe(float(input('Höhe >>>'))))
