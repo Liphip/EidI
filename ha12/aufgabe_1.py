879b493ab56a1eee3e21f8a73668ffc5c629c9f8
Luis Michaelis <school@lmichaelis.de>
2020-02-04 17:00:47 +0100

    Add HA12

--- ha12/aufgabe_1.py
+++ ha12/aufgabe_1.py
@@ -1,7 +1,5 @@
-import random
-
-
 class Mensch(object):
+
     def __init__(self, name, weiblich):
         self.name = name
         self.weiblich = weiblich
@@ -17,151 +15,154 @@ class Mensch(object):
 
 
 class Gallier(Mensch):
-    def __init__(self, wildschweine, name, weiblich):
+    def __init__(self, name, weiblich):
         super().__init__(name, weiblich)
-        self._wildschweine = wildschweine
-        if (not self.name.endswith("ine") and weiblich) or (not self.name.endswith("ix") and not weiblich):
-            self.name += "ine" if weiblich else "ix"
+        self.wildschweine_gegessen = 0
+
+        if weiblich and not name.endswith('ine'):
+            self.name = name + 'ine'
+        elif not weiblich and not name.endswith('ix'):
+            self.name = name + 'ix'
 
     def set_name(self, neuername):
         self.name = neuername
-        if (not self.name.endswith("ine") and self.get_weiblich()) or (
-                not self.name.endswith("ix") and not self.get_weiblich()):
-            self.name += "ine" if self.get_weiblich() else "ix"
+
+        if self.weiblich and not self.name.endswith('ine'):
+            self.name = neuername + 'ine'
+        elif not self.weiblich and not self.name.endswith('ix'):
+            self.name = neuername + 'ix'
 
     def get_wildschweine(self):
-        return self._wildschweine
+        return self.wildschweine_gegessen
 
-    def iss_wildschweine(self):
-        self._wildschweine += 1
+    def iss_wildschwein(self):
+        self.wildschweine_gegessen += 1
 
 
 class Roemer(Mensch):
-    _imperator = None
+    imperator = None
 
-    def __init__(self, losses, name, weiblich):
+    def __init__(self, name, weiblich):
         super().__init__(name, weiblich)
-        self._losses = losses
-        if (not self.name.endswith("a") and weiblich) or (not self.name.endswith("us") and not weiblich):
-            self.name += "a" if weiblich else "us"
-        if Roemer._imperator is None:
-            Roemer._imperator = self
+        self.verloren = 0
+
+        if weiblich and not name.endswith('a'):
+            self.name = name + 'a'
+        elif not weiblich and not name.endswith('us'):
+            self.name = name + 'us'
+
+        if Roemer.imperator is None:
+            Roemer.imperator = self
 
     def set_name(self, neuername):
         self.name = neuername
-        if (not self.name.endswith("a") and self.get_weiblich()) or (
-                not self.name.endswith("us") and not self.get_weiblich()):
-            self.name += "a" if self.get_weiblich() else "us"
+
+        if self.weiblich and not self.name.endswith('a'):
+            self.name = neuername + 'a'
+        elif not self.weiblich and not self.name.endswith('us'):
+            self.name = neuername + 'us'
 
     def verliere(self):
-        self._losses += 1
+        self.verloren += 1
 
     def wie_oft_verloren(self):
-        return self._losses
+        return self.verloren
 
     def werde_imperator(self):
-        Roemer._imperator = self
+        Roemer.imperator = self
 
 
 class Dorf:
-    def __init__(self, bewohner: set, druide: Gallier, barde: Gallier):
-        self._bewohner: set = bewohner
-        self._druide = druide
-        self._barde = barde
-        if self._druide not in self._bewohner:
-            self._bewohner.add(druide)
-        if self._barde not in self._bewohner:
-            self._bewohner.add(barde)
+    def __init__(self, bewohner, druide, barde):
+        self.bewohner = bewohner
+        self.druide = druide
+        self.barde = barde
 
-    def get_druide(self):
-        return self._druide
+    def set_barde(self, barde):
+        self.barde = barde
+
+        if barde not in self.bewohner:
+            self.bewohner.add(barde)
 
     def get_barde(self):
-        return self._barde
+        return self.barde
+
+    def set_druide(self, druide):
+        self.druide = druide
+
+        if druide not in self.bewohner:
+            self.bewohner.add(druide)
+
+    def get_druide(self):
+        return self.druide
 
     def get_bewohner(self):
-        return self._bewohner
-
-    def set_druide(self, druide: Gallier):
-        self._druide = druide
-        if druide not in self._bewohner:
-            self._bewohner.add(druide)
-
-    def set_barde(self, barde: Gallier):
-        self._barde = barde
-        if barde not in self._bewohner:
-            self._bewohner.add(barde)
+        return self.bewohner
 
 
 class Legion:
-    def __init__(self, soldaten: set, zenturio: Roemer):
-        self._soldaten = set()
-        for each in soldaten:
-            if not each.get_weiblich():
-                self._soldaten.add(each)
-        self._zenturio = zenturio
+    def __init__(self, soldaten, zenturio):
+        self.zenturio = zenturio
+        self.soldaten = soldaten
 
     def get_zenturio(self):
-        return self._zenturio
+        return self.zenturio
 
-    def get_soldaten(self):
-        return self._soldaten
-
-    def set_zenturio(self, zenturio: Roemer):
-        self._zenturio = zenturio
+    def set_zenturio(self, zenturio):
+        self.zenturio = zenturio
 
     def rekrutiere(self, ein_roemer: Roemer):
-        if ein_roemer.get_weiblich() == False and not ein_roemer in self._soldaten and not ein_roemer is self._zenturio:
-            self._soldaten.add(ein_roemer)
+        if ein_roemer.weiblich or ein_roemer == self.zenturio:
+            return
 
-    def pensioniere(self, ein_roemer: Roemer):
-        if ein_roemer in self._soldaten:
-            self._soldaten.remove(ein_roemer)
+        self.soldaten.add(ein_roemer)
+
+    def pensioniere(self, ein_roemer):
+        if ein_roemer in self.soldaten:
+            self.soldaten.remove(ein_roemer)
 
 
 def wettkampf(ein_dorf: Dorf, eine_legion: Legion):
-    if len(ein_dorf.get_bewohner()) == len(eine_legion.get_soldaten()):
-        iGallier = 0
-        for iRoemer in range(len(eine_legion.get_soldaten())):
-            if iGallier >= len(ein_dorf.get_bewohner()) - 1:
-                print("Gallier " + list(ein_dorf.get_bewohner())[
-                    random.randint(0, len(ein_dorf.get_bewohner()) - 1)].get_name() + " misst sich mit Roemer " +
-                      list(eine_legion.get_soldaten())[iRoemer].get_name())
-                list(eine_legion.get_soldaten())[iRoemer].verliere()
-            else:
-                print("Gallier " + list(ein_dorf.get_bewohner())[iGallier].get_name() + " misst sich mit Roemer " +
-                      list(eine_legion.get_soldaten())[iRoemer].get_name())
-                list(eine_legion.get_soldaten())[iRoemer].verliere()
-                iGallier += 1
+    gallier = list(ein_dorf.bewohner)
+    roemer = list(eine_legion.soldaten)
+    roemer.append(eine_legion.zenturio)
 
-    print(
-        "Gallier " + ein_dorf.get_barde().get_name() + " misst sich mit Roemer " + eine_legion.get_zenturio().get_name())
-    eine_legion.get_zenturio().verliere()
+    for i in range(max(len(gallier), len(roemer))):
+        r = roemer[i % len(roemer)]
+        print(f'Gallier {gallier[i % len(gallier)].name} misst sich mit {"Roemer" if r != eine_legion.zenturio else "Zenturio"} {r.name}')
+        roemer[i % len(roemer)].verliere()
 
-    for gallier in ein_dorf.get_bewohner():
-        if not gallier is ein_dorf.get_barde():
-            gallier.iss_wildschweine()
+    for g in ein_dorf.bewohner:
+        if g != ein_dorf.barde:
+            g.iss_wildschwein()
 
 
 # Nicht veraendern!
 if __name__ == '__main__':
 
+
     # Loesen Sie Aufgabenteil g) hier:
+    Laureline = Gallier('Laurel', True)
+    Canine = Gallier('Canine', True)
+    Apfelsine = Gallier('Apfelsine', True)
 
-    Laureline, Canine, Apfelsine = Gallier(0, "Laurel", True), Gallier(0, "Canine", True), Gallier(0, "Apfelsine", True)
-    Praefix, Infix, Postfix = Gallier(0, "Praefix", False), Gallier(0, "Infix", False), Gallier(0, "Postf", False)
-    Salta, Mendoza, Ushuaia = Roemer(0, "Salt", True), Roemer(0, "Mendoza", True), Roemer(0, "Ushuaia", True)
-    Primus, Secundus, Tertius, Quartus, Quintus = Roemer(0, "Primus", False), Roemer(0, "Secundus", False), Roemer(0,
-                                                                                                                   "Tertius",
-                                                                                                                   False), Roemer(
-        0, "Quartus", False), Roemer(0, "Quint", False)
-    Oelixdorf, Bekdorf = Dorf({Laureline, Apfelsine, Praefix}, Laureline, Praefix), Dorf({Laureline, Postfix, Infix},
-                                                                                         Infix, Canine)
-    Hispana = Legion({Quintus, Quartus, Tertius, Mendoza}, Salta)
+    Praefix = Gallier('Praefix', False)
+    Infix = Gallier('Infix', False)
+    Postfix = Gallier('Postf', False)
 
-    # print(Laureline.get_name(), Canine.get_name(), Apfelsine.get_name(), Praefix.get_name(), Infix.get_name(),
-    #      Postfix.get_name(), Salta.get_name(), Mendoza.get_name(), Ushuaia.get_name(), Primus.get_name(),
-    #      Secundus.get_name(), Tertius.get_name(), Quartus.get_name(), Quintus.get_name())
+    Salta = Roemer('Salt', True)
+    Mendoza = Roemer('Mendoza', True)
+    Ushuaia = Roemer('Ushuaia', True)
+
+    Primus = Roemer('Primus', False)
+    Secundus = Roemer('Secundus', False)
+    Tertius = Roemer('Tertius', False)
+    Quartus = Roemer('Quartus', False)
+    Quintus = Roemer('Quint', False)
+
+    Oelixdorf = Dorf({Laureline, Apfelsine, Praefix}, Laureline, Praefix)
+    Bekdorf = Dorf({Laureline, Postfix, Infix, Canine}, Infix, Canine)
+    Hispana = Legion({Quintus, Quartus, Tertius}, Salta)
 
     # Diesen Teil duerfen Sie nicht veraendern
 
@@ -177,12 +178,12 @@ if __name__ == '__main__':
     if not (Primus.get_name() == "Primus" and Quintus.get_name() == "Quintus"):
         print("Fehler: Roemer falsch benannt")
 
-    if not (Roemer._imperator == Salta):
+    if not (Roemer.imperator == Salta):
         print("Fehler: Imperator falsch")
 
     Primus.werde_imperator()
 
-    if not Roemer._imperator == Primus:
+    if not Roemer.imperator == Primus:
         print("Fehler: Thronfolge kaputt")
 
     Laureline.set_name("Geraldine")
