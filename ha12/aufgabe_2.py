ae5b3c816032221d91a18654ee62a9ba87116f40
Liphip <57034143+Liphip@users.noreply.github.com>
2020-02-05 17:02:27 +0100

    Fixed ha12

--- ha12/aufgabe_2.py
+++ ha12/aufgabe_2.py
@@ -1,49 +1,49 @@
 class Vliste:
     def __init__(self, eintrag, nachfolger):
-        self.eintrag = eintrag
-        self.nachfolger: Vliste = nachfolger
+        self._eintrag = eintrag
+        self._nachfolger: Vliste = nachfolger
 
     def gleich(self, other):
-        if self.eintrag == other.eintrag:
-            if self.nachfolger is not None and other.nachfolger is not None:
-                if self.nachfolger.gleich(other.nachfolger):
+        if self._eintrag == other.eintrag:
+            if self._nachfolger is not None and other.nachfolger is not None:
+                if self._nachfolger.gleich(other.nachfolger):
                     return True
-            elif self.nachfolger == other.nachfolger:
+            elif self._nachfolger == other.nachfolger:
                 return True
         return False
 
     def append(self, entry):
-        if self.nachfolger is None:
-            self.nachfolger = Vliste(entry, None)
+        if self._nachfolger is None:
+            self._nachfolger = Vliste(entry, None)
         else:
-            self.nachfolger.append(entry)
+            self._nachfolger.append(entry)
 
     def extend(self, entry):
-        if self.nachfolger is None:
-            self.nachfolger = entry
+        if self._nachfolger is None:
+            self._nachfolger = entry
         else:
-            self.nachfolger.extend(entry)
+            self._nachfolger.extend(entry)
 
     def filter(self, fct):
-        if fct(self.eintrag):
-            return Vliste(self.eintrag, self.nachfolger.filter(fct)) if self.nachfolger is not None else Vliste(
-                self.eintrag, None)
+        if fct(self._eintrag):
+            return Vliste(self._eintrag, self._nachfolger.filter(fct)) if self._nachfolger is not None else Vliste(
+                self._eintrag, None)
         else:
-            return self.nachfolger.filter(fct)
+            return self._nachfolger.filter(fct)
 
     def map(self, fct):
-        return Vliste(fct(self.eintrag), self.nachfolger.map(fct)) if self.nachfolger is not None else Vliste(
-            fct(self.eintrag), None)
+        return Vliste(fct(self._eintrag), self._nachfolger.map(fct)) if self._nachfolger is not None else Vliste(
+            fct(self._eintrag), None)
 
     def nachListe(self):
-        return [self.eintrag] + self.nachfolger.nachListe() if self.nachfolger is not None else [self.eintrag]
+        return [self._eintrag] + self._nachfolger.nachListe() if self._nachfolger is not None else [self._eintrag]
 
-    def debug(self, ret: str = "", depth: int = 0):
-        depth += 1
-        if self.nachfolger is None:
-            return ret + " ( " + str(self.eintrag) + " )" + (depth - 1) * ")"
-        else:
-            return self.nachfolger.debug(ret + " ( " + str(self.eintrag), depth)
+    #def debug(self, ret: str = "", depth: int = 0):
+    #    depth += 1
+    #    if self._nachfolger is None:
+    #        return ret + " ( " + str(self._eintrag) + " )" + (depth - 1) * ")"
+    #    else:
+    #        return self._nachfolger.debug(ret + " ( " + str(self._eintrag), depth)
 
 
 def nachVliste(list: list):
@@ -67,5 +67,4 @@ if __name__ == '__main__':
     print(testList2.nachListe())
     print(Vliste(1, Vliste(2, Vliste(3, None))).filter(lambda x: x % 2 == 1).debug())
     print(Vliste(1, Vliste(2, Vliste(3, None))).map(lambda x: x + 2).debug())
-    print(testList.debug(), testList2.debug(), testList3.debug(), testList4.debug(), testList5.debug(),
-          testList6.debug(), testList7.debug(), sep="\n")
+    #print(testList.debug(), testList2.debug(), testList3.debug(), testList4.debug(), testList5.debug(), testList6.debug(), testList7.debug(), sep="\n")
